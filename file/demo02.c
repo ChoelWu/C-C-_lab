74576616b331661ca32cd2b755d9cfcb05a2c30d
ChoelWu <38238531+ChoelWu@users.noreply.github.com>
2018-11-26 18:02:46 +0800

    C

--- file/demo02.c
+++ file/demo02.c
@@ -13,22 +13,38 @@ int iterationB(int data, struct Node *B);
 
 struct Node *getC(struct Node *A, struct Node *B);
 
-int main() {
-    struct Node *A = NULL, *B = NULL, *a = NULL, *b=NULL;
-    int flag = 1, input;
-    A = a = (struct Node*)malloc(sizeof(struct Node));
-    printf("请输入A集合的元素，-1结束输入");
-    while(flag) {
-        scanf("%d", &input);
-        if(input == -1) {
-            flag = 0;
-        } else {
-            a->data = input;
-            a->next = (struct Node*)malloc(sizeof(struct Node));
-        }
-    }
+int outputC(struct Node *C);
 
-    getC(A, B);
+int main() {
+    struct Node *A = NULL, *B = NULL, *C = NULL, *a = NULL, *b = NULL;
+    int flag1 = 1, flag2 = 1, input;
+    A = a = (struct Node *) malloc(sizeof(struct Node));
+    printf("请输入A集合的元素，-1结束输入\n");
+    do {
+        scanf("%d", &input);
+        if (input == -1) {
+            flag1 = 0;
+        } else {
+            a->next = (struct Node *) malloc(sizeof(struct Node));
+            a = a->next;
+            a->data = input;
+            a->next = NULL;
+        }
+    } while (flag1);
+    printf("集合A输入完成，请输入A集合的元素，-1结束输入\n");
+    do {
+        scanf("%d", &input);
+        if (input == -1) {
+            flag2 = 0;
+        } else {
+            b->next = (struct Node *) malloc(sizeof(struct Node));
+            b = b->next;
+            b->data = input;
+            b->next = NULL;
+        }
+    } while (flag2);
+    C = getC(A, B);
+    outputC(C);
     return 0;
 }
 
@@ -60,5 +76,9 @@ int iterationB(int data, struct Node *B) {
             q = q->next;
         };
     }
+    return 0;
+}
+
+int outputC(struct Node *C) {
     return 0;
 }
